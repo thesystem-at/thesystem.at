deee58c6db28479f94dc75f399c41cb8ba1a5e0b
jascha <jascha@jaeh.at>
2020-06-30 16:14:08 +0200

    update theme

--- src/assets/themes/system/index.mjs
+++ src/assets/themes/system/index.mjs
@@ -10,23 +10,176 @@ export const vars = {
   },
 
   neutral: '#212121',
+
+  widths: {
+    tablet: '500px',
+    laptop: '730px',
+    desktop: '940px',
+    agency: '1200px',
+  },
 }
 
 export default (v = {}) => {
   v = { ...vars, ...v }
 
   return {
+    body: {
+      fontSize: '18px',
+      fontFamily: 'sans-serif',
+      overflowX: 'initial',
+      lineHeight: 1.2,
+    },
+
+    '.Page': {
+      h1: {
+        padding: '2em 0',
+        fontWeight: 'bold',
+        position: 'relative',
+        width: '100%',
+
+        p: {
+          fontSize: '1em',
+          lineHeight: 1,
+
+          '&:last-child': {
+            fontSize: '2em',
+          },
+        },
+      },
+    },
+
     '.Wrapper': {
       width: '100%',
+      maxWidth: 'inherit',
     },
 
     '.hr.litto': {
-      right: 0,
       position: 'relative',
+      width: '100%',
+      display: 'inline-block',
     },
 
-    '.location': {
-      clear: 'both',
+    '.sections': {
+      display: 'inline-block',
+      margin: '0 0 0 1em',
+
+      section: {
+        clear: 'both',
+        margin: '0 3vw 3em',
+        width: '33vw',
+      },
+
+      '.description, .location': {
+        fontSize: '0.8em',
+      },
+    },
+
+    '.Footer': {
+      padding: 0,
+      position: 'relative',
+      margin: '4em 0 0 2vw',
+      width: 'auto',
+
+      a: {
+        display: 'inline-block',
+      },
+
+      img: {
+        width: '2.5em',
+        margin: '0 2em 0 0',
+      },
+    },
+
+    [`@media screen and (min-width: ${v.widths.tablet})`]: {
+      body: {
+        fontSize: '25px',
+      },
+
+      '.sections': {
+        section: {
+          margin: '0 4vw 0 0',
+          float: 'right',
+        },
+        '.presents, .location': {
+          marginBottom: '2em',
+        },
+        '.description': {
+          width: '33vw',
+          marginTop: 0,
+        },
+      },
+    },
+
+    [`@media screen and (min-width: ${vars.widths.laptop})`]: {
+      '.VideoContainer': {
+        width: '50vw',
+        height: '28.125vw',
+        float: 'right',
+        margin: '2vw 2vw 0 0',
+        top: 0,
+        right: 0,
+
+        img: {
+          maxWidth: '100%',
+          maxHeight: '100%',
+        },
+      },
+
+      '.hr.litto': {
+        clear: 'none',
+        margin: '-3em 0 0',
+
+        '.Polkadot': {
+          left: 0,
+        },
+
+        img: {
+          margin: '1vw 0 0 2vw',
+          float: 'left',
+        },
+
+        '.PreviewImage': {
+          width: '35vw',
+        },
+      },
+
+      '.Page': {
+        h1: {
+          width: 'auto',
+
+          p: {
+            fontSize: '3vw',
+
+            '&:last-child': {
+              fontSize: '5.5vw',
+            },
+          },
+        },
+      },
+
+      '.sections': {
+        float: 'right',
+        margin: '7em 0 0',
+        width: '40vw',
+
+        section: {
+          float: 'left',
+          width: '23vw',
+          clear: 'none',
+        },
+      },
+    },
+    [`@media screen and (min-width: ${vars.widths.desktop})`]: {
+      '.sections': {
+        margin: '7em 0 0',
+        width: '40vw',
+
+        section: {
+          float: 'left',
+          margin: '0 2em 0 0',
+          width: 'auto',
+        },
+      },
     },
   }
 }
