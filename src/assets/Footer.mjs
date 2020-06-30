0300acf0e0a208731f8667a29ce90f9b6255bc83
jascha <jascha@jaeh.at>
2020-06-30 13:24:05 +0200

    Footer: add title and koer

--- src/assets/Footer.mjs
+++ src/assets/Footer.mjs
@@ -1,5 +1,16 @@
 export const View = () =>
   footer({ class: 'Footer' }, [
-    Link({ to: 'https://magic.github.io' }, Img({ title: 'made with a few bits of magic', src: '/social/magic.png' })),
-    Link({ to: 'https://amwasser.wien' }, Img({ title: 'yppshop provided by usus.wien', src: '/social/usus.png' })),
+    h3('Support:'),
+
+    Link({ to: 'https://koer.or.at' }, Img({ title: 'funded by kör <3', src: '/social/koer.png' })),
+
+    Link(
+      { to: 'https://usus.wien' },
+      Img({ title: 'yppshop provided by usus.wien', src: '/social/usus.png' }),
+    ),
+
+    Link(
+      { to: 'https://magic.github.io' },
+      Img({ title: 'made with a few bits of magic', src: '/social/magic.png' }),
+    ),
   ])
