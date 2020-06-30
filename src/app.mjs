95a4d632f3303c42acc5a0d8de9bd9b1f919aff7
jascha <jascha@jaeh.at>
2020-06-30 13:23:48 +0200

    app: change date

--- src/app.mjs
+++ src/app.mjs
@@ -1,4 +1,4 @@
 export const state = {
   title: 'THESYSTEM',
-  description: 'THESYSTEM - INTRO - 04.07.2020 - 22:00 - Yppenplatz 5, 1160 Wien',
+  description: 'THESYSTEM - INTRO - 11.07.2020 - 22:00 - Yppenplatz 5, 1160 Wien',
 }
