ada4dde2e63a6692c9178b7a404841a1ee9d6f7c
jascha <jascha@jaeh.at>
2020-07-03 21:56:43 +0200

    change date

--- src/app.mjs
+++ src/app.mjs
@@ -1,4 +1,5 @@
 export const state = {
   title: 'THESYSTEM',
-  description: 'THESYSTEM - INTRO - 11.07.2020 - 22:00 - Yppenplatz 5, 1160 Wien',
+  description: 'THESYSTEM - July / August 2020 - Yppenplatz 5, 1160 Wien',
+  // description: 'THESYSTEM - INTRO - 11.07.2020 - 22:00 - Yppenplatz 5, 1160 Wien',
 }
--- src/assets/Location.mjs
+++ src/assets/Location.mjs
@@ -1,7 +1,7 @@
 export const View = () =>
   section({ class: 'location' }, [
-    p({ class: 'date' }, '11.07.2020'),
-    p({ class: 'time' }, '22:00'),
+    p({ class: 'date' }, 'July 2020'),
+    // p({ class: 'time' }, '22:00'),
     p({ class: 'street' }, 'Yppenplatz 5'),
     p({ class: 'district' }, '1160 Wien'),
   ])
