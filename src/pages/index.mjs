664ebb16f4ccee42402d7f699f5a69545292fb0c
jascha <jascha@jaeh.at>
2020-08-08 00:17:50 +0200

    format

--- src/pages/index.mjs
+++ src/pages/index.mjs
@@ -12,10 +12,7 @@ export const View = state => [
 
   Slogan({ subTitle: 'Yppenplatz 5' }),
 
-
-div({ class: 'manifest' }, [
-    p('August 2020'),
-  ]),
+  div({ class: 'manifest' }, [p('August 2020')]),
 
   ExhibitionWrapper({
     title: 'The human body…',
@@ -51,7 +48,7 @@ div({ class: 'manifest' }, [
         text: 'WARBLAYDR',
         to: 'https://primitur.at',
       },
-      middle: 'performs', 
+      middle: 'performs',
       post: '11.08. 18:00',
     },
     date: '',
@@ -77,15 +74,15 @@ div({ class: 'manifest' }, [
     description: [
       'Series framing my work for de-colonialism. ',
       'Colour is no available fund for distinguishing anything. ',
-      "What is prefered tells a story about the individual person and nothing else. ",
-      "Looking after historical perspectives is looking for unfairness. ",
-      "This work examines the beginning of Rock‘n Roll, which came out of the rhythm of Black music. ",
+      'What is prefered tells a story about the individual person and nothing else. ',
+      'Looking after historical perspectives is looking for unfairness. ',
+      'This work examines the beginning of Rock‘n Roll, which came out of the rhythm of Black music. ',
       'It‘s Elvis, who won a „game“, because he had the right colour of skin. ',
-      "The hidden winner could have been the black people, who still are persecuted by lot‘s of whites, thinking, they‘ve got to fight a war, full of hostility. ",
-      ],
+      'The hidden winner could have been the black people, who still are persecuted by lot‘s of whites, thinking, they‘ve got to fight a war, full of hostility. ',
+    ],
   }),
 
-    ExhibitionWrapper({
+  ExhibitionWrapper({
     title: 'The Wound',
     img: {
       src: '/selina.png',
@@ -106,15 +103,12 @@ div({ class: 'manifest' }, [
       'An echo in the empty space ',
       "A shrine to meditate over what's left ",
       'After the anthropocene',
-      ],
+    ],
   }),
 
-  div({ class: 'manifest' }, [
-    p('September 2020'),
-    p('tba'),
-  ]),
+  div({ class: 'manifest' }, [p('September 2020'), p('tba')]),
 
-// manifestoooooo
+  // manifestoooooo
 
   div({ class: 'manifest' }, [
     h1('Manifest'),
@@ -124,11 +118,8 @@ div({ class: 'manifest' }, [
       "We don't need space, time is irrelevant, objects stay as useful as the user. ",
     ]),
     h5(
-      "Involved collectives: Dachsbau, Sandkasten Syndikat, Setzkasten Wien, Zirkus Mops, Villa Schapira ",
+      'Involved collectives: Dachsbau, Sandkasten Syndikat, Setzkasten Wien, Zirkus Mops, Villa Schapira ',
     ),
-    h5(
-      'Concept: Litto / Daniela Weiss, Text: WARBLAYDR', 
-    ),
+    h5('Concept: Litto / Daniela Weiss, Text: WARBLAYDR'),
   ]),
-
 ]
