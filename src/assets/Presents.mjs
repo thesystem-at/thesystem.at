7b19dbac00953ec95bc369468f463df400b6802d
ms litto <litto.eu@gmail.com>
2020-08-09 23:01:16 +0200

    Presents: add link to middle

--- src/assets/Presents.mjs
+++ src/assets/Presents.mjs
@@ -1,6 +1,6 @@
 export const View = state =>
   section({ class: 'presents' }, [
     state.pre && p(state.pre.to ? Link(state.pre) : state.pre),
-    state.middle && p(state.middle),
+    state.middle && p(state.middle.to ? Link(state.middle) : state.middle),
     state.post && p(state.post.to ? Link(state.post) : state.post),
   ])
