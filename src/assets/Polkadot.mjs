d09bbff2a0ab50cefa5c21f3eeebfafd239a11ca
jascha <jascha@jaeh.at>
2020-08-09 19:18:46 +0200

    Polkadot: no top margin

--- src/assets/Polkadot.mjs
+++ src/assets/Polkadot.mjs
@@ -6,7 +6,6 @@ export const style = {
   height: '100%',
   width: '50vw',
   right: 0,
-  top: '3em',
   backgroundImage: 'radial-gradient(black 1px, transparent 1px)',
   backgroundSize: '20px 20px',
 }
