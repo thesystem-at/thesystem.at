945daaf634d3d1e9de84b6e15cf8c6e49fd5f508
jascha <jascha@jaeh.at>
2020-08-10 12:52:55 +0200

    add preview-image

--- src/app.mjs
+++ src/app.mjs
@@ -1,12 +1,7 @@
 export const state = {
   title: 'THESYSTEM',
   description: 'THESYSTEM - August – Oktober 2020 - Yppenplatz 5, 1160 Wien',
-  // description: 'THESYSTEM - INTRO - 11.07.2020 - 22:00 - Yppenplatz 5, 1160 Wien',
-
-  // languages: [
-  //   { code: 'en', to: '/', text: 'english' },
-  //   { code: 'de', to: '/de/', text: 'deutsch' },
-  // ],
+  image: 'https://thesystem.at/thesystem_preview_image.jpg',
 
   pageClass: 'light',
 }
