a7604ad90a414000963c77ca5e473a3a0e4e4d6e
jascha <jascha@jaeh.at>
2020-11-23 18:45:02 +0100

    config: fix WEB_ROOT

--- config.mjs
+++ config.mjs
@@ -1,6 +1,6 @@
 export default {
   ROOT: 'src',
-  WEB_ROOT: 'yppenplatz5',
+  WEB_ROOT: '/yppenplatz5/',
   URL: 'thesystem.at',
   PUBLIC: '../docs/yppenplatz5/',
   THEME: '@thesystemcollective/magic-theme',
